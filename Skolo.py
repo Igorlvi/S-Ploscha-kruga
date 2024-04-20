cb4377f030f9b57c26043602388eeafc6bb32d3b
Igorlvi <167427557+Igorlvi@users.noreply.github.com>
2024-04-20 18:24:30 +0300

    Update Skolo.py

--- Skolo.py
+++ Skolo.py
@@ -4,13 +4,31 @@
 
 from math import sqrt, pi
 import turtle
+import matplotlib.pyplot as plt
 
-print("Розрахуйте S площу круга")
+print("Розрахуйте S площу круга , та L довжину його кола ")
 
 r = float(input("Введіть радіус кола R ? = "))
+
 print ("S круга = Пі(3.14)*(R в квадраті)")
-print("Площа: %.2f" % (pi * r ** 2))
+print ("L кола = 2*Пі(3.14)*R ")
+print("S Площа = %.2f" % (pi * r ** 2))
+print("L Довжина = %.2f" % (2*pi * r ))
+print ("Поки Ви звіряєте свій розв'язок із розв'язком даної програми, черепашка та matplotlib накреслили Вам ваше коло . Знайдіть їх у себе на моніторі.")
+
+# turtle
 turtle.left(90)
 turtle.circle(r*37)
-print ("Поки Ви звіряєте свій розв'язок із розв'язком даної програми, черепашка намалювала Вам ваш круг. Знайдіть його у себе на моніторі.")
+turtle.hideturtle()
 
+# matplotlib
+plt.figure(figsize=(r+2,r+2))
+circle1=plt.Circle(xy=(0.0,0.0), radius= r, color='grey', linewidth=1,fill = False)
+fig = plt.gcf()
+ax = fig.gca()
+ax.add_patch(circle1)
+plt.xlim([-1.2*r,r+2])
+plt.ylim([-1.2*r,r+2])
+plt.savefig('circle2.png',dpi=600)
+plt.title('Ваше коло накреслене на координатній сітці')
+plt.show()
